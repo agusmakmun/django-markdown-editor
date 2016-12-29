bbd48c67a8638c1d559e704a915d7b0808776aae
agusmakmun <summon.agus@gmail.com>
2016-12-29 19:37:26 +0700

    initial v1.0.0

--- README.rst
+++ README.rst
@@ -1,7 +1,8 @@
-draceditor
-------------
+draceditor |pypi version|
+------------------------------
 
-.. image:: https://img.shields.io/pypi/v/draceditor.svg?style=flat-square
+.. |pypi version|
+   image:: https://img.shields.io/pypi/v/draceditor.svg?style=flat-square
    :target: https://pypi.python.org/pypi/draceditor
 
 .. image:: https://img.shields.io/badge/license-GNUGPLv3-blue.svg?style=flat-square
@@ -16,6 +17,5 @@ draceditor
 .. image:: https://img.shields.io/pypi/dm/draceditor.svg?style=flat-square
    :target: https://pypi.python.org/pypi/draceditor
 
--------------------
 
-    **Django Markdown Editor** build for Dracos Linux https://dracos-linux.org *(Under Development Mode)*
+**DracEditor** is Django Markdown Editor build for Dracos Linux https://dracos-linux.org *(Under Development Mode)*
--- draceditor/static/js/draceditor.js
+++ draceditor/static/js/draceditor.js
@@ -114,11 +114,83 @@
       });
     };
 
+    /**
+     * The state of CodeMirror at the given position.
+     * https://github.com/lepture/editor
+     */
+    var getState = function(cm, pos) {
+      pos = pos || cm.getCursor('start');
+      var stat = cm.getTokenAt(pos);
+      if (!stat.type) return {};
+
+      var types = stat.type.split(' ');
+
+      var ret = {}, data, text;
+      for (var i = 0; i < types.length; i++) {
+        data = types[i];
+        if (data === 'strong') {
+          ret.bold = true;
+        } else if (data === 'variable-2') {
+          text = cm.getLine(pos.line);
+          if (/^\s*\d+\.\s/.test(text)) {
+            ret['ordered-list'] = true;
+          } else {
+            ret['unordered-list'] = true;
+          }
+        } else if (data === 'atom') {
+          ret.quote = true;
+        } else if (data === 'em') {
+          ret.italic = true;
+        }
+      }
+      return ret;
+    }
+
+    var replaceSelection = function(cm, active, start, end) {
+      var text;
+      var startPoint = cm.getCursor('start');
+      var endPoint = cm.getCursor('end');
+      if (active) {
+        text = cm.getLine(startPoint.line);
+        start = text.slice(0, startPoint.ch);
+        end = text.slice(startPoint.ch);
+        cm.setLine(startPoint.line, start + end);
+      } else {
+        text = cm.getSelection();
+        cm.replaceSelection(start + text + end);
+
+        startPoint.ch += start.length;
+        endPoint.ch += start.length;
+      }
+      cm.setSelection(startPoint, endPoint);
+      cm.focus();
+    }
+
     var onKeyUpEvent = function(e) {
       console.log(e);
       onMention();
       onEmoji();
     }
+
+    var timeout;
+    var update = function(e) {
+        console.log(e);
+        onMention();
+        console.log(e.getValue());
+        //clearTimeout(timeout);
+        //timeout = setTimeout(getMarkdown, 1000);
+    };
+
+    var editor;
+    setTimeout(function(){
+      //$('.CodeMirror').attr({'contentEditable': 'true'});
+      editor = $('.CodeMirror')[0].CodeMirror;
+      editor.on('change', update);
+      editor.on('keyup', function(cm, e) {
+        console.log(cm, e);
+        onMention();
+      });
+    }, 500);
 
     var draceditor = $(this);
     var dracEditor = $(this).find('.draceditor');
