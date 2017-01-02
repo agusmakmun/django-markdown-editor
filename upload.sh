dd0a888a87d7fba930d19c6f4aa443cc8d6b1b79
agusmakmun <summon.agus@gmail.com>
2017-01-02 23:52:55 +0700

    added bash upload

---
+++ upload.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Command to upload new version release at pypi
+# ./upload.sh pypi
+if [ "$1" == "pypi" ]; then
+    python setup.py sdist upload -r pypi
+fi
+
+if [ "$1" == "git" ]; then
+
+    # Command to upload new tag version
+    # by checking the last version release.
+    # ./upload.sh git tag
+    if [ "$2" == "tag" ]; then
+        # check last release version and plus it with 1
+        # eg: 1.0.8 => 1.0.9
+        new_version=$(
+          git tag | tail -n1 | awk -F '[/.]' '{ gsub("v", "", $1); print $1$2$3 + 1}' | fold -w1 | paste -sd.
+        );
+        # echo "git tag -a v$new_version -m 'Release v$new_version'";
+        git tag -a "v$new_version" -m "Release v$new_version";
+        git push origin "v$new_version";
+        echo "Current version: v$new_version";
+    fi
+
+    # Command to push new git commit
+    # ./upload.sh git commit
+    if [ "$2" == "commit" ]; then
+        echo -n "Commit name > ";
+        read commit_name
+
+        if [ "$commit_name" ]; then
+            git add .;
+            git commit -m "$commit_name";
+            git push -u origin master;
+        fi
+    fi
+fi
