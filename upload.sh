bebf62010551a53a053c70639d8d3dc95278c4ee
agusmakmun <summon.agus@gmail.com>
2017-01-03 01:17:24 +0700

    fixed bash new version

--- upload.sh
+++ upload.sh
@@ -14,10 +14,10 @@ if [ "$1" == "git" ]; then
     if [ "$2" == "tag" ]; then
         # check last release version and plus it with 1
         # eg: v1.0.8 => v1.0.9
-        new_version=$(
-          git tag | tail -n1 | awk -F '[/.]' '{ gsub("v", "", $1); print $1$2$3 + 1}' | fold -w1 | paste -sd.
-        );
-        # new_version = $(git tag | tail -n1 | perl -pe 's/\v//g;s/\.//g;$_++;s/(\d)/$1./g;s/.$/\n/;') # v1.0.9 => v1.1.0
+        # new_version=$(
+        #  git tag | tail -n1 | awk -F '[/.]' '{ gsub("v", "", $1); print $1$2$3 + 1}' | fold -w1 | paste -sd.
+        # );
+        new_version = $(git tag | tail -n1 | perl -pe 's/\v//g;s/\.//g;$_++;s/(\d)/$1./g;s/.$/\n/;') # v1.0.9 => v1.1.0
         # echo "git tag -a v$new_version -m 'Release v$new_version'";
         git tag -a "v$new_version" -m "Release v$new_version";
         git push origin "v$new_version";
