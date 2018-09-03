f87fa9fce68597de9414d055e06eb8581fd4a20e
agusmakmun <summon.agus@gmail.com>
2018-09-03 08:16:48 +0700

    added runtests

--- martor/tests/tests.py
+++ martor/tests/tests.py
@@ -5,5 +5,5 @@ from django.test import TestCase
 class SimpleTest(TestCase):
 
     def test_me(self):
-        response = self.client.get('/', follow=True)
+        response = self.client.get('/test-form-view/')
         self.assertEqual(response.status_code, 200)
---
+++ runtests.py
@@ -0,0 +1,51 @@
+import django
+import sys
+import os
+from django.conf import settings
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+settings.configure(
+    DEBUG=True,
+    DATABASES={
+        'default': {
+            'NAME': ':memory:',
+            'ENGINE': 'django.db.backends.sqlite3',
+        }
+    },
+    TEMPLATES=[{
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'django-markdown-editor/martor/templates'),
+            os.path.join(BASE_DIR, 'django-markdown-editor/martor/tests/templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    }],
+    ROOT_URLCONF='martor.tests.urls',
+    INSTALLED_APPS=['django.contrib.auth',
+                    'django.contrib.contenttypes',
+                    'django.contrib.sessions',
+                    'django.contrib.admin',
+                    'martor'])
+
+try:
+    # Django <= 1.8
+    from django.test.simple import DjangoTestSuiteRunner
+    test_runner = DjangoTestSuiteRunner(verbosity=1)
+except ImportError:
+    # Django >= 1.8
+    django.setup()
+    from django.test.runner import DiscoverRunner
+    test_runner = DiscoverRunner(verbosity=1)
+
+failures = test_runner.run_tests(['martor'])
+if failures:
+    sys.exit(failures)
