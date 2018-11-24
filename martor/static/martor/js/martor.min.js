1b08067a7d8b4a0feb816eaa6fb7c19c923631fa
agusmakmun <summon.agus@gmail.com>
2018-11-25 05:26:25 +0700

    fixed #51

--- martor/static/martor/js/martor.min.js
+++ martor/static/martor/js/martor.min.js
@@ -1,5 +1,5 @@
 /**
- * Name         : Martor v1.3.4
+ * Name         : Martor v1.3.5
  * Created by   : Agus Makmun (Summon Agus)
  * Release date : 03-Nov-2018
  * License      : GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
