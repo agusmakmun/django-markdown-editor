651ccbfd00b30ffcef6220e0758d3f06c4d0a5d6
Summon Agus <summon.agus@gmail.com>
2020-11-01 14:07:50 +0700

    update preview & push command

--- push.sh
+++ push.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
 echo '';
+echo " a. development";
+echo " b. master";
+echo -n "[+] branch [a/B] ➜ ";
+read branch;
+
 echo -n "[+] commit ➜ ";
 read commit;
 
-if [ "$commit" ]; then
-    git add .;
-    git commit -m "$commit";
-    git push -u origin master;
+if [ "$commit" ] && [ "$branch" ]; then
+  git add .;
+  git commit -m "$commit";
+
+  if [ "$branch" == 'b' ] || [ "$branch" == 'B' ]; then
+    git checkout master;
+    git push origin master;
+  else
+    git checkout development;
+    git push origin development;
+  fi
+
+  echo "[i] successfully pushed at" $(date);
+else
+  echo "[!] not pushed!";
 fi
