6e00a70ddd47a0fe6f54d914d3bc721d76ee59b0
Swicegood <jguru108@gmail.com>
2021-04-10 22:32:26 -0400

    Update urls.py

--- martor_demo/app/urls.py
+++ martor_demo/app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from app.views import (home_redirect_view, simple_form_view,
-                       post_form_view, test_markdownify)
+from app.views import (overview_view)
 
 urlpatterns = [
     path('', home_redirect_view, name='home_redirect'),
