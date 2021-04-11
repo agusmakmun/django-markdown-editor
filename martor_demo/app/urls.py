b92e4a20c5ad47d14b4d2204623d740a34f79cd9
Swicegood <jguru108@gmail.com>
2021-04-10 22:03:21 -0400

    update for face-detection

--- martor_demo/app/urls.py
+++ martor_demo/app/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
+efrom django.urls import path
 
 from app.views import (home_redirect_view, simple_form_view,
                        post_form_view, test_markdownify)
 
 urlpatterns = [
     path('', home_redirect_view, name='home_redirect'),
-    path('simple-form/', simple_form_view, name='simple_form'),
-    path('post-form/', post_form_view, name='post_form'),
-    path('test-markdownify/', test_markdownify, name='test_markdownify'),
+    path('overview/', simple_form_view, name='simple_form'),
+    
 ]
