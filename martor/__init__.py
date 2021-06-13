41e907ca4e93500b3bdc8e9b3fea7ece4aa38cb5
Summon Agus <summon.agus@gmail.com>
2021-06-13 11:36:54 +0700

    bump version

--- martor/__init__.py
+++ martor/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__VERSION__ = '1.6.2'
+__VERSION__ = '1.6.3'
 __AUTHOR__ = 'Agus Makmun (Summon Agus)'
 __AUTHOR_EMAIL__ = 'summon.agus@gmail.com'
--- martor/static/martor/js/martor.bootstrap.js
+++ martor/static/martor/js/martor.bootstrap.js
@@ -1,7 +1,7 @@
 /**
- * Name         : Martor v1.6.2
+ * Name         : Martor v1.6.3
  * Created by   : Agus Makmun (Summon Agus)
- * Release date : 07-Apr-2021
+ * Release date : 13-Jun-2021
  * License      : GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
  * Repository   : https://github.com/agusmakmun/django-markdown-editor
 **/
