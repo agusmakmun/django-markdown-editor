d9c4b153c759ea187bb47580bb3110747529dee9
Agus Makmun <agus@practical-devsecops.com>
2021-12-16 08:22:35 +0700

    bump version

--- martor/static/martor/js/martor.bootstrap.min.js
+++ martor/static/martor/js/martor.bootstrap.min.js
@@ -1,7 +1,7 @@
 /**
- * Name         : Martor v1.6.5
+ * Name         : Martor v1.6.6
  * Created by   : Agus Makmun (Summon Agus)
- * Release date : 18-Nov-2021
+ * Release date : 16-Dec-2021
  * License      : GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
  * Repository   : https://github.com/agusmakmun/django-markdown-editor
 **/
