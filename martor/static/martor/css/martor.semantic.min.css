76872a62218ae69dc77c3a204ee3ef8359c33aba
Agus Makmun <agus@practical-devsecops.com>
2021-12-19 08:29:27 +0700

    fix: merge gettex_lazy, bump version

--- martor/static/martor/css/martor.semantic.min.css
+++ martor/static/martor/css/martor.semantic.min.css
@@ -1,7 +1,7 @@
 /**
- * Name         : Martor v1.6.6
+ * Name         : Martor v1.6.7
  * Created by   : Agus Makmun (Summon Agus)
- * Release date : 16-Dec-2021
+ * Release date : 19-Dec-2021
  * License      : GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
  * Repository   : https://github.com/agusmakmun/django-markdown-editor
 **/
