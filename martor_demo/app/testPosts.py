6723247cd44ca0d7a40e771646207f39ec4bde69
masnapp <masnapp2224@gmail.com>
2022-06-18 16:46:11 +0000

    edited style

--- martor_demo/app/testPosts.py
+++ martor_demo/app/testPosts.py
@@ -1,11 +1,13 @@
 # app/testPosts.py
 
+
 from django.test import TestCase
 from app.models import Post
 
+
 class PostTestCase(TestCase):
-	def testPost(self):
-		post = Post(title="My Title", description="Blurb", wiki="Post Body")
-		self.assertEqual(post.title, "My Title")
-		self.assertEqual(post.description, "Blurb")
-		self.assertEqual(post.wiki, "Post Body")
+ def testPost(self):
+  post = Post(title="My Title", description="Blurb", wiki="Post Body")
+  self.assertEqual(post.title, "My Title")
+  self.assertEqual(post.description, "Blurb")
+  self.assertEqual(post.wiki, "Post Body")
