977dbf01193af6f2209cb545545ecb1d181d41c8
masnapp <masnapp2224@gmail.com>
2022-06-18 17:29:02 +0000

    syntax

--- martor_demo/app/testPosts.py
+++ martor_demo/app/testPosts.py
@@ -1,6 +1,7 @@
 # app/testPosts.py
 from django.test import TestCase
 from app.models import Post
+
 class PostTestCase(TestCase):
     def testPost(self):
         post = Post(title="My Title", description="Blurb", wiki="Post Body")
