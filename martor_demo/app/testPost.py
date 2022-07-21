63f5ad5ca85805fef944ff405e0905047102e70a
Marco <mmanrique.dev@gmail.com>
2022-07-20 21:26:44 -0500

    add unit test and static files

---
+++ martor_demo/app/testPost.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+from app.models import Post
+
+
+class PostTestCase(TestCase):
+    def testPost(self):
+        post = Post(title="My Title", description="Blurb", wiki="Post Body")
+        self.assertEqual(post.title, "My Title")
+        self.assertEqual(post.description, "Blurb")
+        self.assertEqual(post.wiki, "Post Body")
