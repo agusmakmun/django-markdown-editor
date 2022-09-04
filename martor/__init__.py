8a3b8a0b5ad9cd358b9ba13ba7e8f0b52c5c51f6
Agus Makmun <agus@practical-devsecops.com>
2022-09-04 22:09:23 +0700

    feat: added command to automatic upgrade package

--- martor/__init__.py
+++ martor/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 __VERSION__ = "1.6.14"
+__RELEASE_DATE__ = "04-Sep-2022"
 __AUTHOR__ = "Agus Makmun (Summon Agus)"
 __AUTHOR_EMAIL__ = "summon.agus@gmail.com"
--- push.sh
+++ push.sh
@@ -4,24 +4,69 @@ echo '';
 echo " a. development";
 echo " b. master";
 echo -n "[+] branch [a/B] ➜ ";
-read branch;
+read -r branch;
 
 echo -n "[+] commit ➜ ";
-read commit;
+read -r commit;
+
+# package folder & name
+package=martor
 
 if [ "$commit" ] && [ "$branch" ]; then
-  git add .;
-  git commit -m "$commit";
+    if [ "$branch" == 'b' ] || [ "$branch" == 'B' ]; then
+        echo -n "[+] upgrade version? [y/N] ➜ ";
+        read -r upgrade_version;
 
-  if [ "$branch" == 'b' ] || [ "$branch" == 'B' ]; then
-    git checkout master;
-    git push origin master;
-  else
-    git checkout development;
-    git push origin development;
-  fi
+        if [ "$upgrade_version" == 'y' ] || [ "$upgrade_version" == 'Y' ]; then
+            # https://stackoverflow.com/a/3250387/6396981
+            old_version=$(grep '__VERSION__ = ".*"' $package/__init__.py | cut -d '"' -f 2);
+            old_release_date=$(grep '__RELEASE_DATE__ = ".*"' $package/__init__.py | cut -d '"' -f 2);
+            echo "[i] current version is $old_version, released at $old_release_date";
 
-  echo "[i] successfully pushed at" $(date);
+            last_numb_old_version=$(echo "$old_version" | grep -o .$);
+            last_numb_new_version=$(( last_numb_old_version + 1 ))
+
+            suggested_version="${old_version%?}${last_numb_new_version}"
+            echo -n "[+] type new version [default:$suggested_version] ➜ ";
+            read -r input_new_version;
+
+            if [ ! "$input_new_version" ]; then
+              input_new_version=$suggested_version;
+            fi
+
+            # 04-Sep-2022
+            new_release_date=$(date +%d-%b-%Y);
+
+            # https://stackoverflow.com/a/525612/6396981
+            # sed -i -e => for linux user
+            # sed -i "" => for mac user
+            echo "[i] updating new version..."
+            find $package/ -type f \( -name "*.py" -or -name "*.css" -or -name "*.js" \) -exec sed -i "" "s/$old_version/$input_new_version/g" {} \;
+            find $package/ -type f \( -name "*.py" -or -name "*.css" -or -name "*.js" \) -exec sed -i "" "s/$old_release_date/$new_release_date/g" {} \;
+        fi
+
+        git add .;
+        git commit -m "$commit";
+        git checkout master;
+        git push origin master;
+
+        if [ "$upgrade_version" == 'y' ] || [ "$upgrade_version" == 'Y' ]; then
+            echo "[i] preparing upload to pypi..."
+            if ! [ -x "$(command -v twine)" ]; then
+                pip install twine
+            fi
+
+            python setup.py sdist
+            twine upload dist/"$package-$input_new_version".tar.gz
+        fi
+    else
+        git add .;
+        git commit -m "$commit";
+        git checkout development;
+        git push origin development;
+    fi
+
+    echo "[i] successfully pushed at $(date)";
 else
-  echo "[!] not pushed!";
+    echo "[!] not pushed!";
 fi
