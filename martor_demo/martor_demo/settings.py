060be778d84468e8cb95f7c3f66ee6e2a2476db3
Glenn Dixon <glenn.dixon@groups360.com>
2023-02-23 09:42:51 -0600

    add unit test and static files

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = os.path.join(BASE_DIR, "static")
+MEDIA_URL = os.path.join(BASE_DIR, "media")
 STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
 MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
