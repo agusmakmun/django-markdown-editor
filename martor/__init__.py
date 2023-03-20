6ce259453c88d96d405e008f5d03655923eb7461
Agus Makmun <agus@practical-devsecops.com>
2023-03-20 07:27:13 +0700

    fix: failed to build docker image on push to dockerhub job v2

--- martor/__init__.py
+++ martor/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__VERSION__ = "1.6.21"
+__VERSION__ = "1.6.22"
 __RELEASE_DATE__ = "20-Mar-2023"
 __AUTHOR__ = "Agus Makmun (Summon Agus)"
 __AUTHOR_EMAIL__ = "summon.agus@gmail.com"
