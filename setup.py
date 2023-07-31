1c2b27343545f5a9842230ce572d4a6553e62abd
tomaszn <tomaszn@users.noreply.github.com>
2023-07-31 20:27:08 +0000

    Drop support for Python 3.6, enable support for Markdown 3.4

--- setup.py
+++ setup.py
@@ -32,7 +32,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: JavaScript',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
