0c4bfda18478515db8e405b1024ca7dd0ad89e7e
Spikey <rafal.paciorek19@gmail.com>
2024-01-11 22:44:01 +0100

    change setting of staticfiles

--- martor_demo/martor_demo/settings.py
+++ martor_demo/martor_demo/settings.py
@@ -143,5 +143,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(tempfile.gettempdir(), 'martor_static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_ROOT = os.path.join(tempfile.gettempdir(), 'martor_media')
