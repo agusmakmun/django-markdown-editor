11d07b223c74b06d0362b65e8ff1a94ae86650ec
Agus Makmun <agus@practical-devsecops.com>
2024-03-09 21:58:52 +0700

    fix: version already pushed to pypi

--- martor/__init__.py
+++ martor/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__VERSION__ = "1.6.28"
+__VERSION__ = "1.6.36"
 __RELEASE_DATE__ = "09-Mar-2024"
 __AUTHOR__ = "Agus Makmun (Summon Agus)"
 __AUTHOR_EMAIL__ = "summon.agus@gmail.com"
