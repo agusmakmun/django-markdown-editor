1071626dff3669debf7139da12c0ce0a64760640
Agus Makmun <agus@practical-devsecops.com>
2024-03-09 20:57:21 +0700

    fix: release workflow

--- martor/static/martor/js/martor.bootstrap.min.js
+++ martor/static/martor/js/martor.bootstrap.min.js
@@ -1,5 +1,5 @@
 /**
- * Name         : Martor v1.6.30
+ * Name         : Martor v1.6.31
  * Created by   : Agus Makmun (Summon Agus)
  * Release date : 09-Mar-2024
  * License      : GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
