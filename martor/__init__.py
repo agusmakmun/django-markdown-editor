30fa257a929e9b40ebd9ad41b152d5fc116c4a22
Agus Makmun <agus@practical-devsecops.com>
2024-03-24 10:07:28 +0700

    feat: bump version to compatible with Django 5.0 #236

--- martor/__init__.py
+++ martor/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__VERSION__ = "1.6.38"
-__RELEASE_DATE__ = "15-Mar-2024"
+__VERSION__ = "1.6.39"
+__RELEASE_DATE__ = "24-Mar-2024"
 __AUTHOR__ = "Agus Makmun (Summon Agus)"
 __AUTHOR_EMAIL__ = "summon.agus@gmail.com"
--- martor/static/martor/css/martor.semantic.min.css
+++ martor/static/martor/css/martor.semantic.min.css
@@ -1,7 +1,7 @@
 /**
- * Name         : Martor v1.6.38
+ * Name         : Martor v1.6.39
  * Created by   : Agus Makmun (Summon Agus)
- * Release date : 15-Mar-2024
+ * Release date : 24-Mar-2024
  * License      : GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
  * Repository   : https://github.com/agusmakmun/django-markdown-editor
 **/
