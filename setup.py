8d3a024fb369c1d0771b0bb3e54354efb44bd662
Agus Makmun <agus@practical-devsecops.com>
2024-03-29 21:44:31 +0700

    fix: drop support for python 3.7

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: JavaScript",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
