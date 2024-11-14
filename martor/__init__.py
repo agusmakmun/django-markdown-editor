3e3146c47edb16dc92f4c4aabf7bf4202b734361
agusmakmun <summon.agus@gmail.com>
2024-11-15 00:37:22 +0700

    feat: bump version

--- martor/__init__.py
+++ martor/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = "1.6.44"
-__RELEASE_DATE__ = "15-Apr-2024"
+__VERSION__ = "1.6.45"
+__RELEASE_DATE__ = "15-Nov-2024"
 __AUTHOR__ = "Agus Makmun (Summon Agus)"
 __AUTHOR_EMAIL__ = "summon.agus@gmail.com"
